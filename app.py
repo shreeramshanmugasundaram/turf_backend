b0ba0fedad4c61d8f0391aa35ecd5721e9c0c1ce
shreeramshanmugasundaram <shreeramsundaram1@gmail.com>
2025-02-26 13:24:56 +0530

    add home page

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Vedashree! Your Flask app is running on the VPS."
+    return "Hello, Vedashree! Your Flask app is running on the VPS. This is the home page."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5003)  # Runs on port 8000
