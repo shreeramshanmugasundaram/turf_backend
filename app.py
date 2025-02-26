0f783a3efc827b9f686066af147be5c65ec1aef7
shreeramshanmugasundaram <shreeramsundaram1@gmail.com>
2025-02-26 13:20:03 +0530

    port change

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def home():
     return "Hello, Vedashree! Your Flask app is running on the VPS."
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)  # Runs on port 8000
+    app.run(host='0.0.0.0', port=5003)  # Runs on port 8000
