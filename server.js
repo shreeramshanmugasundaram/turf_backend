976e2b723c66163391c7502bb4a10ad0d203a63b
shreeramshanmugasundaram <shreeramsundaram1@gmail.com>
2025-02-26 17:03:11 +0530

    changed to node

---
+++ server.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5003;
+
+// Middleware
+app.use(express.json()); // Parses JSON request body
+app.use(cors()); // Enables CORS
+// Sample API Route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to my Node.js backend!" });
+});
+
+// Start Server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
